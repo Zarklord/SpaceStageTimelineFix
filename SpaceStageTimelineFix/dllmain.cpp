3fa5580553f9adac47d1c2d98162d1250fdeeb79
Zachary Lockwood <bestgoaltenderever@gmail.com>
2024-01-14 23:05:42 -0500

    added SMFX projects, update dllmain

--- SpaceStageTimelineFix/dllmain.cpp
+++ SpaceStageTimelineFix/dllmain.cpp
@@ -2,31 +2,11 @@
 #include "stdafx.h"
 #include "SpaceStageTimelineFix.h"
 
-void Initialize()
-{
-	// This method is executed when the game starts, before the user interface is shown
-	// Here you can do things such as:
-	//  - Add new cheats
-	//  - Add new simulator classes
-	//  - Add new game modes
-	//  - Add new space tools
-	//  - Change materials
-}
-
-void Dispose()
-{
-	// This method is called when the game is closing
-}
-
 void AttachDetours()
 {
 	SpaceStageTimelineFix::AttachDetours();
-	// Call the attach() method on any detours you want to add
-	// For example: cViewer_SetRenderType_detour::attach(GetAddress(cViewer, SetRenderType));
 }
 
-
-// Generally, you don't need to touch any code here
 BOOL APIENTRY DllMain( HMODULE hModule,
                        DWORD  ul_reason_for_call,
                        LPVOID lpReserved
@@ -35,9 +15,6 @@ BOOL APIENTRY DllMain( HMODULE hModule,
 	switch (ul_reason_for_call)
 	{
 	case DLL_PROCESS_ATTACH:
-		ModAPI::AddPostInitFunction(Initialize);
-		ModAPI::AddDisposeFunction(Dispose);
-
 		PrepareDetours(hModule);
 		AttachDetours();
 		CommitDetours();
